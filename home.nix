da9d0f4d3f13563d1aa4980c7d9942885c8e0a99
Kas Buunk <kasbuunk@icloud.com>
2024-06-02 11:08:04 +0200

    style: nest home attributes

--- home.nix
+++ home.nix
@@ -6,182 +6,184 @@
 
   manual.manpages.enable = true;
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  home = {
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    file = {
+      # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+      # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+      # # symlink to the Nix store copy.
+      # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+      # # You can also set the file content immediately.
+      # ".gradle/gradle.properties".text = ''
+      #   org.gradle.console=verbose
+      #   org.gradle.daemon.idletimeout=3600000
+      # '';
+    };
+
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    homeDirectory = "/Users/kasbuunk";
+
+    username = "kasbuunk";
+
+    # nixpkgs.config.allowUnfree = true;
+
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    packages = with pkgs; [
+      # # Adds the 'hello' command to your environment. It prints a friendly
+      # # "Hello, world!" when run.
+      hello
+
+      # # It is sometimes useful to fine-tune packages, for example, by applying
+      # # overrides. You can do that directly here, just don't forget the
+      # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+      # # fonts?
+      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+      # # You can also create simple shell scripts directly inside your
+      # # configuration. For example, this adds a command 'my-hello' to your
+      # # environment:
+      # (pkgs.writeShellScriptBin "my-hello" ''
+      #   echo "Hello, ${config.home.username}!"
+      # '')
+
+      alacritty
+      any-nix-shell
+      (aspellWithDicts (dicts: with dicts; [ nl en en-computers en-science ]))
+      autojump
+      babelfish
+      bash
+      bat
+      cacert
+      clang
+      comma
+      cmake
+      coreutils
+      cowsay
+      delve
+      diffutils
+      docker
+      # unstable.emacs29-macport # Collides with programs.emacs.
+      fd
+      fish
+      fzf
+      fx
+      gci
+      gnused
+      glab
+      go
+      gofumpt
+      gopkgs
+      gomodifytags
+      gopls
+      gotest
+      gotests
+      gotools
+      golangci-lint
+      go-outline
+      go-mockery
+      go-tools
+      graphviz
+      grpcurl
+      home-manager
+      htop
+      imagemagick
+      impl
+      inetutils
+      inkscape
+      istioctl
+      jq
+      kind
+      kubectl
+      kubectx
+      kubecolor
+      kubernetes-helm
+      languagetool
+      libtool
+      libxml2
+      lolcat
+      lua
+      lua-language-server
+      natscli
+      neovim
+      nmap
+      nodejs_21
+      obsidian
+      openssl
+      plantuml
+      pdf2svg
+      perl
+      pgcli
+      pkg-config
+      postgresql_16
+      python3
+      readline
+      ripgrep
+      rustup
+      sbcl
+      sqlite
+      swiProlog
+      terminal-notifier
+      terraform # Non-free.
+      terraform-ls
+      texlive.combined.scheme-full
+      tflint
+      thefuck
+      tree-sitter
+      ttyd
+      typescript
+      ugrep
+      vim
+      watch
+      wget
+      xmlsec
+      xorg.libXext
+      yarn
+      yq-go
+      zig
+    ];
+
+    # preferXdgDirectories = true; # Error for unknown reasons.
+
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. If you don't want to manage your shell through Home
+    # Manager then you have to manually source 'hm-session-vars.sh' located at
+    # either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/kasbuunk/etc/profile.d/hm-session-vars.sh
+    #
+    sessionVariables = {
+      EDITOR = "emacs";
+      DIRENV_LOG_FORMAT = "";
+    };
+
+    shellAliases = {
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      "....." = "cd ../../../..";
+      "......" = "cd ../../../../..";
+    };
+
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "23.11"; # Please read the comment before changing.
   };
-
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.homeDirectory = "/Users/kasbuunk";
-
-  home.username = "kasbuunk";
-
-  # nixpkgs.config.allowUnfree = true;
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-
-    alacritty
-    any-nix-shell
-    (aspellWithDicts (dicts: with dicts; [ nl en en-computers en-science ]))
-    autojump
-    babelfish
-    bash
-    bat
-    cacert
-    clang
-    comma
-    cmake
-    coreutils
-    cowsay
-    delve
-    diffutils
-    docker
-    # unstable.emacs29-macport # Collides with programs.emacs.
-    fd
-    fish
-    fzf
-    fx
-    gci
-    gnused
-    glab
-    go
-    gofumpt
-    gopkgs
-    gomodifytags
-    gopls
-    gotest
-    gotests
-    gotools
-    golangci-lint
-    go-outline
-    go-mockery
-    go-tools
-    graphviz
-    grpcurl
-    home-manager
-    htop
-    imagemagick
-    impl
-    inetutils
-    inkscape
-    istioctl
-    jq
-    kind
-    kubectl
-    kubectx
-    kubecolor
-    kubernetes-helm
-    languagetool
-    libtool
-    libxml2
-    lolcat
-    lua
-    lua-language-server
-    natscli
-    neovim
-    nmap
-    nodejs_21
-    openssl
-    plantuml
-    pdf2svg
-    perl
-    pgcli
-    pkg-config
-    postgresql_16
-    python3
-    readline
-    ripgrep
-    rustup
-    sbcl
-    sqlite
-    swiProlog
-    terminal-notifier
-    terraform # Non-free.
-    terraform-ls
-    texlive.combined.scheme-full
-    tflint
-    thefuck
-    tree-sitter
-    ttyd
-    typescript
-    ugrep
-    vim
-    watch
-    wget
-    xmlsec
-    xorg.libXext
-    yarn
-    yq-go
-    zig
-  ];
-
-  # home.preferXdgDirectories = true; # Error for unknown reason.
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/kasbuunk/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "emacs";
-    DIRENV_LOG_FORMAT = "";
-  };
-
-  home.shellAliases = {
-    "..." = "cd ../..";
-    "...." = "cd ../../..";
-    "....." = "cd ../../../..";
-    "......" = "cd ../../../../..";
-  };
-
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
 
   programs = {
     alacritty.enable = true;
@@ -337,7 +339,4 @@
       pictures = "Pictures";
     };
   };
-
-  # error upon installation
-  # programs.firefox.enable = true;
 }
