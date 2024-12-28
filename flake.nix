b34c5c4b709762c40cd91fd9a4a94f7649a0fa0e
Kas Buunk <kasbuunk@icloud.com>
2024-12-28 23:21:55 +0100

    feat: upgrade nix pkgs to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     nix-darwin = {
@@ -11,7 +11,7 @@
     };
 
     homeManager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
